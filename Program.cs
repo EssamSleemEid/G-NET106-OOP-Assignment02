c455e862003d5dc080b706172ddfae7e62fa9f1f
Nongoogle <essamsleem9@gmail.com>
2026-09-19 17:19:33 +0300

    Question02 Part01

--- Program.cs
+++ Program.cs
@@ -14,6 +14,28 @@
 
             //bc its support the inheritance and the polymorphism which make it easier to orgnize and reuse
             #endregion
+
+            #region Question02
+
+            #region Part01
+            //a) Which class is the parent class?
+
+            //shipment
+
+            //b) Which class is the child class?
+
+            //expressShipment
+
+            //c) What members are inherited by ExpressShipment?
+
+            //tracking code
+
+            //d) Why is inheritance better than duplicating the same code in multiple classes?
+
+            //bc its allow us to reuse the code from parent class without writting the same code again in every child class
+            #endregion
+
+            #endregion
         }
     }
 }
